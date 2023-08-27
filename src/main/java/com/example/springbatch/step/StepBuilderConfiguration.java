5cc169dba7566db7ee3d2ed86118839a8cf0a063
hsyoon <bagzein@gmail.com>
2023-08-27 17:06:21 +0900

    - StepBuilderFactory 개념 및 API

---
+++ src/main/java/com/example/springbatch/step/StepBuilderConfiguration.java
@@ -0,0 +1,98 @@
+package com.example.springbatch.step;
+
+import com.example.springbatch.job.CustomJobParameterValidator;
+import com.example.springbatch.job.CustomJobParametersIncrementer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class StepBuilderConfiguration {
+
+  private final JobBuilderFactory jobBuilderFactory;
+
+  private final StepBuilderFactory stepBuilderFactory;
+
+  @Bean
+  public Job batchJob() {
+    return jobBuilderFactory.get("batchJob")
+      .incrementer(new RunIdIncrementer())  // 같은 파라미터로 실행 가능하도록
+      .start(step1())
+      .next(step2())
+      .next(step3())
+      .build();
+  }
+
+  @Bean
+  public Step step1() {
+    return stepBuilderFactory.get("step1")
+      .tasklet((stepContribution, chunkContext) -> {
+        System.out.println("step1 was executed");
+
+        return RepeatStatus.FINISHED;
+      })
+      .build();
+  }
+
+  @Bean
+  public Step step2() {
+    return stepBuilderFactory.get("step2")
+      .<String, String> chunk(3)
+      .reader(() -> null)
+      .processor((ItemProcessor<String, String>) s -> null)
+      .writer(items -> {})
+      .build();
+  }
+
+  @Bean
+  public Step step3() {
+    return stepBuilderFactory.get("step3")
+      .partitioner(step1())
+      .gridSize(2)
+      .build();
+  }
+
+  @Bean
+  public Step step4() {
+    return stepBuilderFactory.get("step4")
+      .job(job())
+      .build();
+  }
+
+  @Bean
+  public Step step5() {
+    return stepBuilderFactory.get("step5")
+      .flow(flow())
+      .build();
+  }
+
+  private Flow flow() {
+    FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+    flowBuilder.start(step2()).end();
+
+    return flowBuilder.build();
+  }
+
+  private Job job() {
+    return jobBuilderFactory.get("job")
+      .start(step1())
+      .next(step2())
+      .next(step3())
+      .build();
+  }
+}
