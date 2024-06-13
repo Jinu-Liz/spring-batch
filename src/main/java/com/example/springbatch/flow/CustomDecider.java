c04e447676929edfcc7750741dd6e8a9ed8bad82
Hyungseock-Yun <bagzein@gmail.com>
2024-06-13 23:46:55 +0900

    - FlowJob > FlowJob 구성 예제

--- src/main/java/com/example/springbatch/flow/CustomDecider.java
+++ src/main/java/com/example/springbatch/flow/CustomDecider.java
@@ -5,9 +5,12 @@ import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
 
+/**
+ * Decider를 사용하여 조건적인 흐름들을 제어할 수 있다.
+ */
 public class CustomDecider implements JobExecutionDecider {
 
-  private int count = 0;
+  private int count = 1;
 
   @Override
   public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
