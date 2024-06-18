d2639fecb5a69cccdd7776077936d5f8d4a008e0
Hyungseock-Yun <bagzein@gmail.com>
2024-06-18 23:58:59 +0900

    - FlowJob 주석 추가

--- src/main/java/com/example/springbatch/flow/FlowJob.java
+++ src/main/java/com/example/springbatch/flow/FlowJob.java
@@ -12,6 +12,9 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * FlowJob 예제
+ */
 @RequiredArgsConstructor
 @Configuration
 public class FlowJob {
